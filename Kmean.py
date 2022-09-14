87e42a1ed4967693e3aa95da55e538e5059f4a1f
laihoiyeen <113412814+laihoiyeen@users.noreply.github.com>
2022-09-14 11:34:29 +0800

    Update Kmean.py

--- Kmean.py
+++ Kmean.py
@@ -1,15 +1,19 @@
-st.write("The Kmean cluster")
-
 file = "mall_customer.csv"
+
 import pandas as pd
 df = pd.read_csv(file)
+
 features = ['Annual_Income_(k$)', 'Spending_Score']
 X = df[features]
+
 plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score']);
+
 from sklearn.cluster import KMeans
 kmeans = KMeans(n_clusters=5)
 kmeans.fit(X)
 y_kmeans = kmeans.predict(X)
+
 plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score'], c=y_kmeans, s=50, cmap='viridis')
+
 centers = kmeans.cluster_centers_
 plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
